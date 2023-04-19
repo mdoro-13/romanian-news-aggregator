eebbf114b6a2359a3442e778f268bfe4b6b4da7e
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2023-04-19 23:24:52 +0300

    Display articles in a grid and add styling

--- UI/script.js
+++ UI/script.js
@@ -1,20 +1,20 @@
 import { fetchArticles } from './http-calls.js';
 
 const articles = await fetchArticles();
-const newsContainer = document.querySelector('.news-container');
+const articleGrid = document.querySelector('.article-grid');
 
 console.log(articles);
 
 articles.forEach((article) => {
   const row = document.createElement('div');
   const articleUrl = `//${article.articleUrl}`;
-  row.classList.add('row');
+  row.classList.add('grid-item');
   
   row.innerHTML = `
-    <div class="col-lg-2"><img class="article-img" src="${article.pictureUrl}" alt="Article image"></div>
-    <div class="col-lg-6"><a href="${articleUrl}" target="_blank">${article.title}</a></div>
-    <div class="col-lg-2">${new Date(article.date).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
-    <div class="col-lg-2">${article.provider}</div>
+    <div><img class="article-img" src="${article.pictureUrl}" alt="Article image"></div>
+    <div><a href="${articleUrl}" target="_blank">${article.title}</a></div>
+    <div class="date">${new Date(article.date).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
+    <div class="provider">${article.provider}</div>
   `;
-  newsContainer.appendChild(row);
+  articleGrid.appendChild(row);
 });
