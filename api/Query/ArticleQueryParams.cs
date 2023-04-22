8310b35aecd503689f75100466087b7083db102e
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2023-04-23 01:07:54 +0300

    Implement keyset pagination on backend

--- api/Query/ArticleQueryParams.cs
+++ api/Query/ArticleQueryParams.cs
@@ -1,32 +1,54 @@
 ﻿using System.Reflection;
+using static Api.Query.ArticleQueryParams;
 
 namespace Api.Query;
 
 public class ArticleQueryParams
 {
-    public ICollection<int> ProviderIds { get; set; }
-    public string Keyword { get; set; }
+    public ICollection<int> ProviderIds { get; init; }
+    public string Keyword { get; init; }
+    public ScrollDirection Direction { get; init; }
+    public string DatePosted { get; init; }
+    public int Count { get; init; } = 100;
 
     public static async ValueTask<ArticleQueryParams?> BindAsync(HttpContext context, ParameterInfo parameter)
     {
         string keyword = context.Request.Query["Keyword"];
+        string beforeDate = context.Request.Query["DatePosted"];
+        string stringScrollDirection = context.Request.Query["Direction"];
         var providerIdsStringVals = context.Request.Query["providerIds"];
+
+        ScrollDirection scrollDirection = (ScrollDirection)Int32.Parse(stringScrollDirection);
 
         if (providerIdsStringVals.Count == 0)
         {
             return new ArticleQueryParams
             {
-                Keyword = keyword
+                Keyword = keyword,
+                DatePosted = beforeDate,
+                Direction = scrollDirection
             };
         }
 
-        var providerIds = providerIdsStringVals.ToString()?.Split(',').Select(int.Parse).ToList();
+        var providerIds = providerIdsStringVals.ToString()?
+            .Split(',')
+            .Select(int.Parse)
+            .ToList();
+
         var result = new ArticleQueryParams
         {
             Keyword = keyword,
-            ProviderIds = providerIds
+            ProviderIds = providerIds,
+            DatePosted = beforeDate,
+            Direction = scrollDirection
         };
 
         return await ValueTask.FromResult<ArticleQueryParams?>(result);
+    }
+
+    public enum ScrollDirection
+    {
+        Down = 0,
+        Up = 1,
     }
 }
--- api/Repository/ArticleRepository.cs
+++ api/Repository/ArticleRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Entities;
 using Api.Query;
 using Npgsql;
+using System.Diagnostics;
+using static Api.Query.ArticleQueryParams;
 
 namespace Api.Repository;
 
@@ -22,52 +24,99 @@ public class ArticleRepository
         using (var connection = new NpgsqlConnection(connectionString))
         {
             await connection.OpenAsync();
-            using (var command = new NpgsqlCommand())
+
+            var command = CreateCommand(connection, queryParams);
+
+            var articles = await ReadArticlesAsync(command);
+
+            return articles;
+        }
+    }
+
+    private NpgsqlCommand CreateCommand(NpgsqlConnection connection, ArticleQueryParams queryParams)
+    {
+        var command = new NpgsqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT a.id, a.title, a.date, a.scrape_date, a.article_url, a.picture_url, p.name as provider_name " +
+                              "FROM articles a " +
+                              "JOIN providers p ON a.provider_id = p.id ";
+
+        if (queryParams.ProviderIds is not null && queryParams.ProviderIds.Any())
+        {
+            FilterByProvider(queryParams, command);
+        }
+
+        if (!string.IsNullOrWhiteSpace(queryParams.Keyword))
+        {
+            FilterByKeyword(queryParams, command);
+        }
+
+        FilterByLowerThanDate(queryParams, command);
+        OrderByDateDescending(command);
+        LimitByCount(queryParams, command);
+
+        Console.WriteLine(command.CommandText);
+        return command;
+    }
+
+    private static void FilterByProvider(ArticleQueryParams queryParams, NpgsqlCommand command)
+    {
+        var providerIdsParameter = string.Join(",", Enumerable.Range(0, queryParams.ProviderIds.Count).Select(i => "@ProviderId" + i));
+        command.CommandText += "WHERE a.provider_id IN (" + providerIdsParameter + ") ";
+        for (int i = 0; i < queryParams.ProviderIds.Count; i++)
+        {
+            command.Parameters.AddWithValue("ProviderId" + i, queryParams.ProviderIds.ElementAt(i));
+        }
+    }
+    private static void FilterByKeyword(ArticleQueryParams queryParams, NpgsqlCommand command)
+    {
+        command.CommandText += queryParams.ProviderIds is not null && queryParams.ProviderIds.Any() ? "AND " : "WHERE ";
+        command.CommandText += "a.title LIKE @Keyword ";
+        command.Parameters.AddWithValue("Keyword", "%" + queryParams.Keyword + "%");
+    }
+
+    private static void FilterByLowerThanDate(ArticleQueryParams queryParams, NpgsqlCommand command)
+    {
+        bool validDate = DateTime.TryParse(queryParams.DatePosted, out var date);
+
+        if (validDate)
+        {
+            string commandText = queryParams.Direction == ScrollDirection.Down ? "AND a.date < @DatePosted " : "AND a.date > @DatePosted ";
+            command.CommandText += commandText;
+            command.Parameters.AddWithValue("DatePosted", date);
+        }
+    }
+    private static void OrderByDateDescending(NpgsqlCommand command)
+    {
+        command.CommandText += "ORDER BY a.date DESC ";
+    }
+    private static void LimitByCount(ArticleQueryParams queryParams, NpgsqlCommand command)
+    {
+        command.CommandText += "LIMIT @Count ";
+        command.Parameters.AddWithValue("Count", queryParams.Count);
+    }
+
+    private async Task<List<Article>> ReadArticlesAsync(NpgsqlCommand command)
+    {
+        var articles = new List<Article>();
+
+        using (var reader = await command.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
             {
-                command.Connection = connection;
-                command.CommandText = "SELECT a.id, a.title, a.date, a.scrape_date, a.article_url, a.picture_url, p.name as provider_name " +
-                                      "FROM articles a " +
-                                      "JOIN providers p ON a.provider_id = p.id ";
-
-                if (queryParams.ProviderIds is not null && queryParams.ProviderIds.Any())
+                articles.Add(new Article
                 {
-                    var providerIdsParameter = string.Join(",", Enumerable.Range(0, queryParams.ProviderIds.Count).Select(i => "@ProviderId" + i));
-                    command.CommandText += "WHERE a.provider_id IN (" + providerIdsParameter + ") ";
-                    for (int i = 0; i < queryParams.ProviderIds.Count; i++)
-                    {
-                        command.Parameters.AddWithValue("ProviderId" + i, queryParams.ProviderIds.ElementAt(i));
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(queryParams.Keyword))
-                {
-                    command.CommandText += queryParams.ProviderIds is not null && queryParams.ProviderIds.Any() ? "AND " : "WHERE ";
-                    command.CommandText += "a.title LIKE @Keyword ";
-                    command.Parameters.AddWithValue("Keyword", "%" + queryParams.Keyword + "%");
-                }
-
-                command.CommandText += "ORDER BY a.date DESC";
-
-                var articles = new List<Article>();
-                using (var reader = await command.ExecuteReaderAsync())
-                {
-                    while (await reader.ReadAsync())
-                    {
-                        articles.Add(new Article
-                        {
-                            Id = reader.GetGuid(0),
-                            Title = reader.GetString(1),
-                            Date = reader.GetDateTime(2),
-                            ScrapeDate = reader.GetDateTime(3),
-                            ArticleUrl = reader.GetString(4),
-                            PictureUrl = reader.GetString(5),
-                            Provider = reader.GetString(6)
-                        });
-                    }
-                }
-
-                return articles;
+                    Id = reader.GetGuid(0),
+                    Title = reader.GetString(1),
+                    Date = reader.GetDateTime(2),
+                    ScrapeDate = reader.GetDateTime(3),
+                    ArticleUrl = reader.GetString(4),
+                    PictureUrl = reader.GetString(5),
+                    Provider = reader.GetString(6)
+                });
             }
         }
+
+        return articles;
     }
 }
