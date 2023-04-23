406f4d7c6c237c061dad8471acf72d8000998d69
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2023-04-23 14:54:42 +0300

    Implement scroll pagination in the UI

--- UI/http-calls.js
+++ UI/http-calls.js
@@ -1,9 +1,10 @@
-export async function fetchArticles() {
-    const endpoint = 'https://localhost:5999/news';
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from endpoint ${endpoint}. Status code: ${response.status}`);
-    }
-    return await response.json();
+export async function fetchArticles(queryConfig) {
+  const endpoint = new URL('https://localhost:5999/news');
+  // endpoint.searchParams.append('keyword', queryConfig.keyword);
+  endpoint.searchParams.append('beforeDate', queryConfig.datePosted);
+  const response = await fetch(endpoint);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data from endpoint ${endpoint}. Status code: ${response.status}`);
   }
-  
+  return await response.json();
+}
--- UI/script.js
+++ UI/script.js
@@ -1,31 +1,63 @@
 import { fetchArticles } from './http-calls.js';
 
-const articles = await fetchArticles();
+let articles = [];
+let lastArticleDate = null;
+let stopScroll = false;
 
-const truncatedArticles = articles.map(article => {
-  if (article.title.length > 90) {
-    return {...article, title: article.title.slice(0, 90) + '...'};
-  } else {
-    return article;
-  }
-});
+const truncateTitle = (title) => {
+  return title.length > 90 ? title.slice(0, 90) + '...' : title;
+};
 
-const articleGrid = document.querySelector('.article-grid');
-
-console.log(articles);
-
-truncatedArticles.forEach((article) => {
-  const row = document.createElement('div');
+const createArticleElement = (article) => {
   const articleUrl = `//${article.articleUrl}`;
+  const row = document.createElement('div');
   row.classList.add('grid-item');
-  
   row.innerHTML = `
     <div class="article-box">
       <div><img class="article-img" src="${article.pictureUrl}" alt="Article image"></div>
-      <div><a href="${articleUrl}" target="_blank">${article.title}</a></div>
+      <div><a href="${articleUrl}" target="_blank">${truncateTitle(article.title)}</a></div>
     </div>
-    <div class="date">${new Date(article.date).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
-    <div class="provider">${article.provider}</div>
+    <div class="info-box">
+      <div class="date-posted">${new Date(article.date).toLocaleDateString('ro-RO', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
+      <div class="provider">${article.provider}</div>
+    </div>
   `;
-  articleGrid.appendChild(row);
-});
+  return row;
+};
+
+const loadArticles = async () => {
+
+  if (!stopScroll) {
+    let newArticles = [];
+    // Load next page
+    const datePosted = lastArticleDate;
+    newArticles = await fetchArticles({ datePosted });
+    if (newArticles.length > 0) {
+      articles = newArticles;
+      lastArticleDate = articles[newArticles.length - 1].date;
+      console.log(articles)
+      console.log(lastArticleDate)
+      const articleElements = newArticles.map(article => createArticleElement(article));
+      articleElements.forEach(articleElement => {
+        articleGrid.appendChild(articleElement);
+      });
+    }
+    else {
+      stopScroll = true;
+    }
+  }
+};
+
+const handleScroll = () => {
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  const triggerAt = document.documentElement.offsetHeight - window.innerHeight - 100;
+  if (scrollPosition > triggerAt) {
+    loadArticles();
+  }
+
+};
+
+const articleGrid = document.querySelector('.article-grid');
+window.addEventListener('scroll', handleScroll);
+loadArticles();
+
