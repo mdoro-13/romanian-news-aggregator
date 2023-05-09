9059fc6a3ffdc61bce834503cce30a431420ad09
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2023-05-10 01:11:10 +0300

    Get api endpoint programatically from UI

--- UI/http-calls.js
+++ UI/http-calls.js
@@ -1,10 +1,23 @@
 export async function fetchArticles(queryConfig) {
-  const endpoint = new URL('http://localhost:5998/news');
-  endpoint.searchParams.append('keyword', queryConfig.keyword);
-  endpoint.searchParams.append('beforeDate', queryConfig.datePosted);
-  const response = await fetch(endpoint);
+  const endpoint = getEndpoint();
+  console.log(`${endpoint}/news`)
+  const apiURL = new URL(`${endpoint}/news`);
+  apiURL.searchParams.append('keyword', queryConfig.keyword);
+  apiURL.searchParams.append('beforeDate', queryConfig.datePosted);
+  const response = await fetch(apiURL);
   if (!response.ok) {
-    throw new Error(`Failed to fetch data from endpoint ${endpoint}. Status code: ${response.status}`);
+    throw new Error(`Failed to fetch data from endpoint ${apiURL}. Status code: ${response.status}`);
   }
   return await response.json();
+}
+
+function getEndpoint() {
+  if (window.location.host.includes('127.0.0.1')) {
+    apiURL = 'http://localhost:5998'
+  }
+
+  // TODO
+  // handle for other environments
+
+  return apiURL;
 }
