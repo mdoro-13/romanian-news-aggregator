51ff069e93999d2dd72e38a06448abfbce01fbf8
mdoro-13 <mihai.dorobantu.dev@gmail.com>
2023-06-07 00:08:14 +0300

    Fix chrome webdriver for the docker container

--- Scrapers/utils/response_handler.py
+++ Scrapers/utils/response_handler.py
@@ -19,7 +19,15 @@ def parse_response(url):
 
         if needs_dynamic_content:
             options = Options()
-            options.add_argument("--headless")  
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--disable-infobars")
+            options.add_argument("--start-maximized")
+            options.add_argument("--disable-notifications")
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            
             driver = webdriver.Chrome(options=options) 
             driver.get(url)
             page_source = driver.page_source
